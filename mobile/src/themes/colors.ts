a5a5f4c2274727ff0b60124d6566c4d278e0dc04
gabrielew <gabrielalcantaratp@gmail.com>
2021-01-25 21:58:04 -0300

    style: adding colors themes

---
+++ mobile/src/themes/colors.ts
@@ -0,0 +1,11 @@
+export default {
+  'header-background': '#1765C0',
+  'container-background': '#e0e0e0',
+  grey: '#f7f7f7',
+  'light-grey': '#EEEEEE',
+  white: '#fff',
+  'temperature-max': '#65A8D1',
+  'temperature-min': '#D16060',
+  'primary-text': '#777777',
+  'default-text': '#000',
+};
