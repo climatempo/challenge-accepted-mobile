aa6531fc30f4a436f293d9b8723f686c412bb7fe
unknown <wallace.dantunes@gmail.com>
2021-02-01 02:15:18 -0300

    adicionando provider do historico

---
+++ src/provider/historicProvider/index.ts
@@ -0,0 +1,18 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {Historic} from '../../model/Historic';
+
+export class HistoricProvider {
+    localStorageKey = 'historicKey';
+    async getHistoric(): Promise<Historic[]> {
+        const json = await AsyncStorage.getItem(this.localStorageKey);
+        if (json === null) {
+            return [];
+        }
+        const parsedJson = JSON.parse(json);
+        if (Array.isArray(parsedJson)) {
+            return parsedJson.map((historic) => ({name: historic.name}));
+        } else {
+            throw new Error('invalid json');
+        }
+    }
+}
