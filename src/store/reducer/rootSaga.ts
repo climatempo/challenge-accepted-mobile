9dc0796e6e54e904f37b2f2f71962a7fafa10548
unknown <wallace.dantunes@gmail.com>
2021-02-02 16:49:24 -0300

    ajustando rootSaga

--- src/store/reducer/rootSaga.ts
+++ src/store/reducer/rootSaga.ts
@@ -1,16 +1,19 @@
 import {all, takeLatest} from 'redux-saga/effects';
 import {HistoricTypes} from './historic/types';
 import {CityTypes} from './city/types';
-import {load} from './historic/sagas';
-import {searchCity} from './city/sagas';
 import {ForecastTypes} from './forecast/types';
-import {getForecast} from './forecast/sagas';
+import {load, removeHistoric, setHistoric} from './historic/sagas';
+import {getForecast, loadLocalStorage} from './forecast/sagas';
+import {searchCity} from './city/sagas';
 
 export default function* rootSaga() {
     return yield all([
         takeLatest(HistoricTypes.LOAD_REQUEST, load),
+        takeLatest(HistoricTypes.SET_HISTORIC_REQUEST, setHistoric),
+        takeLatest(HistoricTypes.REMOVE_HISTORIC_REQUEST, removeHistoric),
         takeLatest(CityTypes.SEARCH_REQUEST, searchCity),
         takeLatest(ForecastTypes.LOAD_REQUEST, getForecast),
+        takeLatest(ForecastTypes.LOAD_LOCALSTORAGE_REQUEST, loadLocalStorage),
     ]);
 }
 1;
